c6640444fa99c737b5332a6ca933665895dbf74a
Br4v3_H3r0 <virusking702@gmail.com>
2020-08-23 13:39:18 +0000

    version 1.0 released

---
+++ server.py
@@ -0,0 +1,135 @@
+
+# Python program to implement server side of chat room. 
+import socket 
+import select 
+import sys 
+from _thread import *
+  
+"""The first argument AF_INET is the address domain of the 
+socket. This is used when we have an Internet Domain with 
+any two hosts The second argument is the type of socket. 
+SOCK_STREAM means that data or characters are read in 
+a continuous flow."""
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+
+
+  
+# # checks whether sufficient arguments have been provided 
+# if len(sys.argv) != 3: 
+#     print "Correct usage: script, IP address, port number"
+#     exit() 
+  
+# takes the first argument from system as IP address 
+host_name = socket.gethostname()
+IP_address = socket.gethostbyname(host_name)  
+  
+# port number 
+Port = 5926
+  
+""" 
+binds the server to an entered IP address and at the 
+specified port number. 
+The client must be aware of these parameters 
+"""
+server.bind((IP_address, Port)) 
+  
+""" 
+listens for 100 active connections. This number can be 
+increased as per convenience. 
+"""
+server.listen(2) 
+print("listening for incoming connections...")
+
+  
+list_of_clients = [] 
+list_of_ips = []
+  
+def clientthread(conn, addr): 
+  
+    # sends a message to the client whose user object is conn 
+    conn.send("Welcome to this chatroom!\n".encode()) 
+    for ip in list_of_ips:
+        text="<" + ip + "> is connected\n"
+        conn.send(text.encode())
+          
+  
+    while True: 
+            try: 
+                message = conn.recv(2048).decode()
+
+                if message=="exit":
+                    message_to_send = "<" + addr[0] + "> has left the chatroom"
+                    broadcast(message_to_send, conn,addr[0])
+                    remove(conn,addr[0]) 
+                    break;
+
+                elif message: 
+  
+                    """prints the message and address of the 
+                    user who just sent the message on the server 
+                    terminal"""
+                    print("<" + addr[0] + "> " + message)
+  
+                    # Calls broadcast function to send message to all 
+                    message_to_send = "<" + addr[0] + "> " + message 
+                    broadcast(message_to_send, conn,addr[0]) 
+                
+
+                else: 
+                    message_to_send = "<" + addr[0] + "> has been disconnected"
+                    broadcast(message_to_send, conn,addr[0])
+                    """message may have no content if the connection 
+                    is broken, in this case we remove the connection"""
+                    remove(conn,addr[0]) 
+                    break;
+  
+            except: 
+                continue
+  
+"""Using the below function, we broadcast the message to all 
+clients who's object is not the same as the one sending 
+the message """
+def broadcast(message, connection,address): 
+    for clients in list_of_clients: 
+        if clients!=connection: 
+            try: 
+                clients.send(message.encode()) 
+            except: 
+                clients.close() 
+  
+                # if the link is broken, we remove the client 
+                remove(clients,address) 
+  
+"""The following function simply removes the object 
+from the list that was created at the beginning of  
+the program"""
+def remove(connection,address=""): 
+    print("<" + address + "> has been disconnected")
+    if connection in list_of_clients: 
+        list_of_clients.remove(connection) 
+    if address in list_of_ips:
+        list_of_ips.remove(address)    
+  
+while True: 
+  
+    """Accepts a connection request and stores two parameters,  
+    conn which is a socket object for that user, and addr  
+    which contains the IP address of the client that just  
+    connected"""
+    conn, addr = server.accept() 
+  
+    """Maintains a list of clients for ease of broadcasting 
+    a message to all available people in the chatroom"""
+    list_of_clients.append(conn) 
+    list_of_ips.append(addr[0])
+  
+    # prints the address of the user that just connected 
+    print("<" + addr[0] + "> connected")
+  
+    # creates and individual thread for every user  
+    # that connects 
+    start_new_thread(clientthread,(conn,addr))     
+  
+conn.close() 
+server.close() 
